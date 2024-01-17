6dd7e73a1a5429a0d972646eb9e825503706bd0d
Your Name <you@example.com>
2024-01-16 21:39:07 -0500

    first commit

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from './assets/chair-01.jpeg'
+import product2 from './assets/chair-02.jpeg'
+import product3 from './assets/chair-03.jpeg'
+import product4 from './assets/chair-04.jpeg'
+import product5 from './assets/chair-05.jpeg'
+import product6 from './assets/chair-06.jpeg'
+import product7 from './assets/chair-07.jpeg'
+import product8 from './assets/chair-08.jpeg'
+import product9 from './assets/chair-09.jpeg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Black chair",
+        price: 199.0,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "White stool",
+        price: 159.0,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "Brown chair",
+        price: 299.0,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "Beige armchair",
+        price: 199.0,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "White armchair",
+        price: 399.0,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "Yellow armchair",
+        price: 459.0,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Leather loveseat",
+        price: 579.0,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Ikea chair",
+        price: 169.0,
+        productImage: product8
+    },
+    {
+        id: 9,
+        productName: "Cozy armchair",
+        price: 369.0,
+        productImage: product9
+    },
+]
